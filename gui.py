fadd73dd631153e5db0723e3acc05e09ee74302e
Jen <jennifer.stemkowski@gmail.com>
2024-04-02 12:01:31 -0400

    Add functionality to compress files on computer

--- gui.py
+++ gui.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from compressmodule import compress, decompress
+from tkinter import filedialog
+
+
+def open_file():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a file to compress")
+    return filename
 
 
 def compression(i, o):
@@ -16,22 +22,8 @@ input_entry = tk.Entry(window, width=50)
 output_entry = tk.Entry(window, width=50)
 
 
-# Labels
-input_label = tk.Label(window, text="File to compress")
-output_label = tk.Label(window, text="Compressed file")
-
-
 # Buttons
-compress_button = tk.Button(window, text="Compress", command=lambda: compression(input_entry.get(), output_entry.get()))
-
-
-# Grid Layout
-input_label.grid(row=0, column=0)
-output_label.grid(row=1, column=0)
-
-input_entry.grid(row=0, column=1)
-output_entry.grid(row=1, column=1)
-
+compress_button = tk.Button(window, text="Compress", command=lambda: compression(open_file(), "compressedoutput1.txt"))
 compress_button.grid(row=2, column=1)
 
 
