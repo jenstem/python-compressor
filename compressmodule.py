1b451bd6808c05b77c808b7c38e81e86c476c046
Jen <jennifer.stemkowski@gmail.com>
2024-04-02 11:03:26 -0400

    Put decompress code into func decompress

--- compressmodule.py
+++ compressmodule.py
@@ -9,4 +9,16 @@ def compress(inputfile, outputfile):
     compressed_file = open(outputfile, 'w')
     compressed_file.write(decoded_data)
 
-compress('demo.txt', 'ot.txt')
+# compress('demo.txt', 'ot.txt')
+
+
+def decompress(inputfile, outputfile):
+    file_content = open(inputfile, 'r').read()
+    encoded_data = file_content.encode('utf-8')
+    decompressed_data = zlib.decompress(base64.b64decode(encoded_data))
+    decoded_data = decompressed_data.decode('utf-8')
+    file = open(outputfile, 'w')
+    file.write(decoded_data)
+    file.close()
+
+decompress('ot.txt', 'dc1.txt')
